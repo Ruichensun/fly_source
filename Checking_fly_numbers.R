fe98bcac59be827e587180d2848cbef1bad1baf0
Ruichensun <ruichensun0@gmail.com>
2018-05-22 14:49:07 -0700

    Updated the code for checking number of flies in each genotype

--- Checking_fly_numbers.R
+++ Checking_fly_numbers.R
@@ -1,16 +1,16 @@
 ##This snippet is to check how many flies have been tested in each types of genotypes
 
-type_of_mutants = length(unique(fly.info$Genotype))
+type_of_mutants = length(unique(fly.info.include$Genotype))
 
 number_of_mutants = c()
-names_of_mutants = unique(fly.info$Genotype)
+names_of_mutants = unique(fly.info.include$Genotype)
 
 for (i in 1:type_of_mutants){
-  number_of_mutants[i] = dim(fly.info[fly.info$Genotype==names_of_mutants[i],])[1]
+  number_of_mutants[i] = dim(fly.info.include[fly.info.include$Genotype==names_of_mutants[i],])[1]
 }
 
 mutant_info = data.frame(names_of_mutants,number_of_mutants)
 
 colnames(mutant_info) = c("Mutant Genotype", "Number of Flies")
 
-write.csv(mutant_info, file = "Mutant_headcount_020918.csv", row.names = FALSE)
+write.csv(mutant_info, file = "Mutant_headcount_052218.csv", row.names = FALSE)
