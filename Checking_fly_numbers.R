0cad88017e2e8f1b1a5c095929007d076dc5d874
Ruichensun <ruichensun0@gmail.com>
2018-07-16 12:13:24 -0700

    Updatede output file date

--- Checking_fly_numbers.R
+++ Checking_fly_numbers.R
@@ -13,4 +13,4 @@ mutant_info = data.frame(names_of_mutants,number_of_mutants)
 
 colnames(mutant_info) = c("Mutant Genotype", "Number of Flies")
 
-write.csv(mutant_info, file = "Mutant_headcount_052218.csv", row.names = FALSE)
+write.csv(mutant_info, file = "Mutant_headcount_071618.csv", row.names = FALSE)
