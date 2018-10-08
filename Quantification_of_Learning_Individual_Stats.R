19681a46c2bd44196a398158464c8025316345b3
Ruichensun <ruichensun0@gmail.com>
2018-10-08 12:44:59 -0700

    Added reference matrix

--- Quantification_of_Learning_Individual_Stats.R
+++ Quantification_of_Learning_Individual_Stats.R
@@ -4,6 +4,41 @@
 input.y.df = data.frame()
 p_value_sum = matrix(nrow = 0, ncol = 9)
 
+#Reference for metric index
+ 
+#   "Number of Pause Starts", #1
+#   "Fraction Time in Pause", #2
+#   "Average Pause Duration", #3
+#   "Max Pause Duration", #4 
+#   "Average Moving Speed ", #5
+#   "Average Moving Speed (excluding pause)", #6
+#   "Average Speed When Enter Pause", #7
+#   "Average Speed When Exit Pause",#8
+#   "Moving Distance Per Minute",#9
+#   "Number of Turns",#10
+#   "Number of Middle Turns",#11
+#   "Fration of Middle Turns Out of Total Turns",#12
+#   "Burstiness (Pause)",#13
+#   "Burstiness (Inter Event Time)",#14
+#   "Burstiness (Scrambled)",#15
+#   "Burstiness (Walking bouts-thresholding)",#16
+#   "Burstiness (Walking events-thresholding)",#17
+#   "Beginning Pause Duration",#18 
+#   "First Pause Duration",#19 
+#   "Transition Probability (Pause not at the end): Pause to Pause",#20
+#   "Transition Probability (Pause not at the end): Pause to Walking",#21
+#   "Transition Probability (Pasue not at the end): Walking to Walking",#22
+#   "Transition Probability (Pause not at the end): Walking to Pause",#23
+#   "Memory",#24
+#   "Memory (inverted)",#25
+#   "Burstiness of Start of Walking (Pause not at the end)",#26 *
+#   "Burstiness of Start of Pause (Pause not at the end)",#27 *
+#   "Average Pause Duration (Pause not at the End)",#28
+#   "Fraction Time in Pause (Pause not at the End)",#29 
+#   "Max Pause Duration (Pause not at the End)", #30
+#   "First Pause Duration (Pause not at the End)" #31
+
+
 sessions <- c(                     
   "E1T1",                   #4
   "E1T1E1",                 #5 
