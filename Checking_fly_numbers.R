453438349129bc005794f33302bb9032900431e1
Ruichensun <ruichensun0@gmail.com>
2018-11-11 18:55:27 -0800

    Updated head count

--- Checking_fly_numbers.R
+++ Checking_fly_numbers.R
@@ -13,4 +13,4 @@ mutant_info = data.frame(names_of_mutants,number_of_mutants)
 
 colnames(mutant_info) = c("Mutant Genotype", "Number of Flies")
 
-write.csv(mutant_info, file = "Mutant_headcount_102618.csv", row.names = FALSE)
+write.csv(mutant_info, file = "Mutant_headcount_110818.csv", row.names = FALSE)
