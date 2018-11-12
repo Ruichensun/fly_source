9ea1ce17591fc47d8fcf245f300f9a664dd6ab95
Ruichensun <ruichensun0@gmail.com>
2018-11-11 20:06:29 -0800

    Updated combine_flyCSV with functions

--- combine_flyCSV.R
+++ combine_flyCSV.R
@@ -1,88 +1,52 @@
-setwd("D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter")
+setwd("D:/Behavioral_project/behavior_experiment_data/Sorted_data_experimenter")
 
-
-combine_flyCSV <- function(input_files, output_file){
+combine_flyCSV <- function(experimenter, type){
   all_info = NULL;
+  input_files = c()
+  for (i in 1:length(experimenter)){
+    filename = paste0("D:/Behavioral_project/behavior_experiment_data/Sorted_data_experimenter/", 
+                      experimenter[i], "/", type[1], "/CSV/Behavioral Experiments - ", type[2], "_", experimenter[i], ".csv")
+    input_files = c(input_files, filename)
+  }
+  
+  output_file = paste0("D:/Behavioral_project/behavior_experiment_data/Sorted_data_experimenter/fly_info_", type[3], ".csv")
   for(i in 1:length(input_files)){
     info = read.csv(input_files[i],header=T,stringsAsFactors=F)
     info$experimenter = experimenter[i]
     info$Fly_Exp = paste(info$Fly,experimenter[i],sep='_')
     all_info = rbind(all_info,info)
   }
-  
   write.table(all_info,
               output_file,
               quote=F,row.names=F,col.names=T,sep=",")  
 }
 
-
 #Mutants
-
-input_files <- c("D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JD/Mutants/CSV/Behavioral Experiments - Mutants_JD.csv",
-                 "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/SW/Mutants/CSV/Behavioral Experiments - Mutants_SW.csv",
-                 "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JG/Mutants/CSV/Behavioral Experiments - Mutants_JG.csv",
-                 "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/ES/Mutants/CSV/Behavioral Experiments - Mutants_ES.csv",
-                 "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/XC/Mutants/CSV/Behavioral Experiments - Mutants_XC.csv"
-                 )
-
-experimenter <- c("JD",
-                  "SW",
-                  "JG",
-                  "ES",
-                  "XC"
-          );
-
-output_file = "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/fly_info_mutants.csv"
+type = c("Mutants","Mutants", "mutants")
+experimenter <- c("JD", "SW", "JG", "ES", "XC")
+combine_flyCSV(experimenter, type)
 
 #CS
-
-input_files<-c("D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/ES/CS/CSV/Behavioral Experiments - CS_ES.csv",
-               "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/RS/CS/CSV/Behavioral Experiments - CS_RS.csv",
-               "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JD/CS/CSV/Behavioral Experiments - CS_JD.csv",
-               "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/SW/CS/CSV/Behavioral Experiments - CS_SW.csv"
-              )
-experimenter <- c(
-  "ES",
-  "RS",
-  "JD",
-  "SW"
-);
-output_file = "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/fly_info_CS.csv"
-
-#CS - Controls
-
-
-input_files <- c("D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JD/CS_controls/CSV/Behavioral Experiments - CS_controls1_JD.csv",
-                 "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JD/CS_controls/CSV/Behavioral Experiments - CS_controls2_JD.csv"
-                 )
-
-experimenter <- c("JD"
-);
-
-output_file = "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/fly_info_CS_controls.csv"
+type = rep("CS", 3)
+experimenter <- c("ES", "RS", "JD","SW")
+combine_flyCSV(experimenter, type)
 
 #Response
-
-input_files<-c(
-  "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JD/Response/CSV/Behavioral Experiments - Response_JD.csv",
-  "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/ES/Response/CSV/Behavioral Experiments - Response_ES.csv"
-)
-experimenter <- c(
-  "JD",
-  "ES"
-);
-output_file = "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/fly_info_response.csv"
+type = c("Response","Response", "response")
+experimenter <- c("JD", "ES")
+combine_flyCSV(experimenter, type)
 
 #Constant_time
+type = rep("CS_constant",3)
+experimenter <- c("JD")
+combine_flyCSV(experimenter, type)
 
-input_files<-c(
-  "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/JD/CS_constant/CSV/Behavioral Experiments - CS_constant_JD.csv"
-)
-experimenter <- c(
-  "JD"
-);
-output_file = "D:/Behavioral_project/Behavior Experiment Data/Sorted_data_experimenter/fly_info_CS_constant.csv"
+#CS - Controls1
+type = c("CS_controls", "CS_controls1", "CS_controls1")
+experimenter <- c("JD")
+combine_flyCSV(experimenter, type)
 
-#Add unique identifier to the master chart
-
-
+#CS - Controls2
+type = c("CS_controls", "CS_controls2", "CS_controls2")
+experimenter <- c("JD")
+combine_flyCSV(experimenter, type)
