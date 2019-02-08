b8db5847216e46d333c11895e0aaffa87bfd304d
Ruichensun <ruichensun0@gmail.com>
2019-02-07 23:57:35 -0800

    Clean up some codes

--- combine_flyCSV.R
+++ combine_flyCSV.R
@@ -8,25 +8,5 @@ combine_flyCSV(experimenter, type)
 
 #CS
 type = rep("CS", 3)
-experimenter <- c("ES", "RS", "JD", "SW", "JGLNRS")
+experimenter <- c("ES", "RS", "JD", "SW")
 combine_flyCSV(experimenter, type)
-
-# #Response
-# type = c("Response","Response", "response")
-# experimenter <- c("JD", "ES")
-# combine_flyCSV(experimenter, type)
-# 
-# #Constant_time
-# type = rep("CS_constant",3)
-# experimenter <- c("JD")
-# combine_flyCSV(experimenter, type)
-# 
-# #CS - Controls1
-# type = c("CS_controls", "CS_controls1", "CS_controls1")
-# experimenter <- c("JD")
-# combine_flyCSV(experimenter, type)
-# 
-# #CS - Controls2
-# type = c("CS_controls", "CS_controls2", "CS_controls2")
-# experimenter <- c("JD")
-# combine_flyCSV(experimenter, type)
