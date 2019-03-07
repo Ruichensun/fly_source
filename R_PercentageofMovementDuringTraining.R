d42e5adc6648d56ab9dfea11b96c901996dbba35
Ruichensun <ruichensun0@gmail.com>
2019-03-07 09:45:12 -0800

    Started working on linear model for coefficient analysis

--- R_PercentageofMovementDuringTraining.R
+++ R_PercentageofMovementDuringTraining.R
@@ -11,9 +11,9 @@ fly.info.movement.R = fly.info.end[((fly.info.end$Genotype == "WT") |
 all_ofs_WT = read.csv("all_ofs_WT.csv", header = T, stringsAsFactors = F)
 
 
-fly.info.movement.T = fly.info.end[(fly.info.end$Genotype == "SUN3") & 
+fly.info.movement.T = fly.info.end[(fly.info.end$Genotype == "D2R-1 x 51635") & 
                                      (fly.info.end$Category =="T"), ]
-fly.info.movement.R = fly.info.end[(fly.info.end$Genotype == "SUN3") & 
+fly.info.movement.R = fly.info.end[(fly.info.end$Genotype == "D2R-1 x 51635") & 
                                      (fly.info.end$Category =="R"), ]
 all_ofs_WT = read.csv("all_ofs_mutants.csv", header = T, stringsAsFactors = F)
 
